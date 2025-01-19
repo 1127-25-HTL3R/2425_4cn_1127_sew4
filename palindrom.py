300a76201464b948f7e28c55510b939274534ea4
1127-25-HTL3R <1127@htl.rennweg.at>
2025-01-19 10:56:05 +0100

    Added 'is_palindrome_sentence' function

--- palindrom.py
+++ palindrom.py
@@ -3,6 +3,7 @@ __author__      = "Felix Friesenbichler"
 __email__       = "1127@htl.rennweg.at"
 __license__     = "GPLv2"
 
+
 def is_palindrom(s: str):
     """
     This method checks if the string s, which contains only one word, is a
@@ -23,3 +24,31 @@ def is_palindrom(s: str):
         return True
     else:
         return False
+
+
+def is_palindrome_sentence(s: str):
+    """
+    Checks is the string s, which may contain a sentence, is a
+    palindrome
+    sentence.
+
+    >>> is_palindrome_sentence("Was it a car or a cat I saw?")
+    True
+
+    >>> is_palindrome_sentence("Do Geese see God?")
+    True
+
+    >>> is_palindrome_sentence("A Toyota")
+    True
+
+    >>> is_palindrome_sentence("Warsaw was raw")
+    True
+    """
+
+    char_list = (" ", ";", "?", ".", ",", ":", "!")
+    string = s.lower()
+
+    for char in char_list:
+        string = string.replace(char, "")
+
+    return is_palindrom(string)
