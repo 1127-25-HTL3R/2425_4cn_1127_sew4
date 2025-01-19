380c2e90c5079deab77fe4ffa777fde54db71161
1127-25-HTL3R <1127@htl.rennweg.at>
2025-01-19 12:20:49 +0100

    Added logic for calculating the MacCarthy-91 function.

--- rekursiv.py
+++ rekursiv.py
@@ -2,3 +2,21 @@
 __author__      = "Felix Friesenbichler"
 __email__       = "1127@htl.rennweg.at"
 __license__     = "GPLv2"
+
+from time import time
+
+def M(n):
+    """
+    Calculates the McCarthy-91 function.
+
+    :param n: Starting value
+    :return: McCarthy Value for n
+    >>> M(101)
+    91
+    >>> M(99)
+    91
+    """
+    if n <= 100:
+        return M(M(n+11))
+    else:
+        return n - 10
