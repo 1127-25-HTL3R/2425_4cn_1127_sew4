0b87c3793fdf542dad3487fd3d29f35d5c827914
1127-25-HTL3R <1127@htl.rennweg.at>
2025-01-19 12:26:37 +0100

    Added remarks for rekursiv.py

--- rekursiv.py
+++ rekursiv.py
@@ -11,12 +11,24 @@ def M(n):
 
     :param n: Starting value
     :return: McCarthy Value for n
-    >>> M(101)
-    91
     >>> M(99)
     91
+    >>> M(150)
+    140
     """
     if n <= 100:
         return M(M(n+11))
     else:
         return n - 10
+
+if __name__ == "__main__":
+    t0 = time()
+    m_list = [M(n) for n in range(200)]
+    m_dict = {n: M(n) for n in range(200)}
+    t1 = time()
+    print(f"Time: {t1-t0:.6f} Seconds")
+
+    # Bemerkungen:
+    # Für n < 100 gibt M(n) immer 91 zurück.
+    # Für n > 100 gibt M(n) immer n - 10.
+    # Es dauert 0.000611 Sekunden zum ausführen
