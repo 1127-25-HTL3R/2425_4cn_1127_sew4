c10f9737333af485af1fa1612e4dae72a03e623f
1127-25-HTL3R <1127@htl.rennweg.at>
2025-02-24 14:58:58 +0100

    UE06: Added logic for get_path_components.py

--- py_adminscripting/get_path_components.py
+++ py_adminscripting/get_path_components.py
@@ -1,4 +1,22 @@
 
-__author__ = "Felix Friesenbichler""
+__author__ = "Felix Friesenbichler"
 __email__ = "1127@htl.rennweg.at"
 __license__ = "GPLv2"
+
+from pathlib import Path
+
+print("-----------------------------------")
+absolute_path = Path(input("Enter the absolute path of a file: "))
+print("-----------------------------------")
+
+print(f"Der Dateiname ohne Verzeichnis: {absolute_path.name}")
+print(f"Der Dateiname ohne die Dateierweiterung: {absolute_path.stem}")
+print(f"Die Dateierweiterung: {absolute_path.suffix}")
+print(f"Der Dateiname ohne Verzeichnis: {absolute_path.anchor}")
+print(f"Das Dateiverzeichnis, das die Datei enthält bzw. das übergeordnete Verzeichnis: {absolute_path.parent}")
+
+
+if absolute_path.parent == absolute_path.parent.parent:
+    print("Parent directory does not exist")
+else:
+    print(absolute_path.parent.parent)
