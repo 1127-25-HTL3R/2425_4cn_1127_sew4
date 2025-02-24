4e86c9acbed7413689758631b45efcd1f8eabd8a
1127-25-HTL3R <1127@htl.rennweg.at>
2025-02-24 15:21:37 +0100

    UE06: Added logic for find_files_by_ext.py

--- py_adminscripting/find_files_by_ext.py
+++ py_adminscripting/find_files_by_ext.py
@@ -2,3 +2,14 @@
 __author__ = "Felix Friesenbichler"
 __email__ = "1127@htl.rennweg.at"
 __license__ = "GPLv2"
+
+from pathlib import Path
+from collections import Counter
+
+path = Path(input("Enter an absolute path: "))
+ext = input("Enter a file extension: ")
+
+print(Counter(pathd.suffix for pathd in Path.cwd().iterdir()))
+
+for file in sorted(path.rglob(f"*.{ext}")):
+    print(file.absolute().resolve())
